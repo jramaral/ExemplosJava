ac74a6f38a19c9aa5676cd7a047c04e7946ce980
jose roberto <j.robertoamaral@gmail.com>
2019-07-04 19:10:41 -0400

    gera tabuada do 1 ao 10 com for

---
+++ tabuada.java
@@ -0,0 +1,23 @@
+package basicojava;
+
+public class tabuada {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for (int i = 1; i <=10; i++) {
+			System.out.println("----------------------" );
+			System.out.println("       TABUADA DO " + i );
+			System.out.println("----------------------" );
+			
+			for (int j = 1; j <=10 ; j++) {
+				
+				System.out.println(i + " X " + j +" = "+ (i*j) );
+			}
+			
+			
+		}
+		
+	}
+}
