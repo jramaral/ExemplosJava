65d07c15f24a223e3875b9f589dcaab2d5b7c940
jose roberto <j.robertoamaral@gmail.com>
2019-07-04 11:45:28 -0400

    Exemplo que mostar o todal de dias vividos

---
+++ DiasDeVidas.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DiasDeVidas {
+
+	private static Scanner teclado;
+	
+	public static void main(String[] args) {
+		
+		teclado = new Scanner(System.in);
+		System.out.println("Digite sua idade: ");
+		int idade = teclado.nextInt();
+		int totalDias = idade * 365;
+		System.out.println("Você tem "+ totalDias + " dias de vida");
+		
+	}
+	
+}
